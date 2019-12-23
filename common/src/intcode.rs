29cc53d65049fd167fdad54e30869a758cfbf2c1
John Shaw <jshaw@shawnation.com>
2019-12-23 00:28:28 -0600

    day09 part 2

--- common/src/intcode.rs
+++ common/src/intcode.rs
@@ -4,6 +4,8 @@ use std::rc::Rc;
 use std::cell::Cell;
 use super::myasync::{Sender, Receiver};
 
+// Old incode computer. Still used by early days. It's broken, but still works
+// for days which use it.
 pub fn run(line: &str, input: &mut VecDeque<i64>) -> Vec<i64> {
     let mut code: HashMap<usize, i64> = line
         .split(",")
@@ -129,41 +131,66 @@ pub fn run(line: &str, input: &mut VecDeque<i64>) -> Vec<i64> {
     output
 }
 
+struct Memory {
+    mem: HashMap<i64, i64>,
+    rb: i64
+}
+
+impl Memory {
+    fn new(code: &str) -> Memory {
+        Memory{
+            mem: code.split(",")
+                .map(|s| s.parse::<i64>().unwrap())
+                .enumerate()
+                .map(|(a, b)| (a as i64, b))
+                .collect(),
+            rb: 0
+        }
+    }
+
+    fn read_arg(&self, mode: i64, arg_pos: i64) -> i64 {
+        self.read(mode, self.mem[&arg_pos])
+    }
+
+    fn write_arg(&mut self, mode: i64, arg_pos: i64, value: i64) {
+        self.write(mode, self.mem[&arg_pos], value)
+    }
+
+    fn read(&self, mode: i64, pos: i64) -> i64 {
+        match mode {
+            0 => self.mem.get(&pos).copied().unwrap_or_default(),
+            1 => pos as i64,
+            2 => self.mem.get(&(self.rb + pos)).copied().unwrap_or_default(),
+            _ => panic!("Invalid parameter mode")
+        }
+    }
+
+    fn write(&mut self, mode: i64, pos: i64, value: i64) {
+        match mode {
+            0 => self.mem.insert(pos, value),
+            2 => self.mem.insert(self.rb + pos, value),
+            _ => panic!("Invalid parameter mode")
+        };
+    }
+
+    fn update_rb(&mut self, d: i64) {
+        self.rb += d;
+    }
+}
+
 pub async fn run_async(line: Rc<String>, mut input: Receiver<i64>, mut output: Sender<i64>) 
     -> (Receiver<i64>, Sender<i64>)
 {
-    let mut code: HashMap<usize, i64> = line
-        .split(",")
-        .map(|s| s.parse::<i64>().unwrap())
-        .enumerate()
-        .collect();
+    let mut mem = Memory::new(&line);
 
-    let mut pc = 0usize; // program counter
-    let rb = Cell::new(0i64); // relative base
-
-    let read = |mem: &HashMap<usize, i64>, mode, pos| {
-        match mode {
-            0 => mem.get(&(pos as usize)).copied().unwrap_or_default(),
-            1 => pos,
-            2 => mem.get(&((rb.get() + pos) as usize)).copied().unwrap_or_default(),
-            _ => panic!("Invalid parameter mode")
-        }
-    };
-
-    let write = |mem: &mut HashMap<usize, i64>, mode, pos: i64, value| {
-        match mode {
-            0 => mem.insert(pos as usize, value),
-            2 => mem.insert((rb.get() + pos) as usize, value),
-            _ => panic!("Invalid parameter mode")
-        }
-    };
+    // program counter
+    let mut pc = 0i64;
 
     loop {
-        let op = *code.entry(pc).or_default();
+        let op = mem.read(0, pc);
         let (mut modes, op) = (op / 100, op % 100);
 
-        //println!("{:?}", (pc..pc+4).map(|n| code[&n]).collect::<Vec<i64>>());
-        println!("pc={} op={}", pc, op);
+        //println!("pc={} op={}", pc, op);
 
         let mut pop_mode = || {
             let m = modes % 10;
@@ -173,87 +200,68 @@ pub async fn run_async(line: Rc<String>, mut input: Receiver<i64>, mut output: S
 
         match op {
             1 => {
-                let (x, y, p) = (code[&(pc+1)], code[&(pc+2)], code[&(pc+3)]);
-                let m1 = pop_mode();
-                let m2 = pop_mode();
-                let m3 = pop_mode();
-                println!("x={} y={} p={} m1={} m2={} m3={}", x, y, p, m1, m2, m3);
-                let v = read(&code, m1, x) + read(&code, m2, y);
-                write(&mut code, m3, p, v);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
+                mem.write_arg(pop_mode(), pc+3, p1 + p2);
                 pc += 4;
             },
             2 => {
-                let (x, y, p) = (code[&(pc+1)], code[&(pc+2)], code[&(pc+3)]);
-                let m1 = pop_mode();
-                let m2 = pop_mode();
-                let m3 = pop_mode();
-                println!("x={} y={} p={} m1={} m2={} m3={}", x, y, p, m1, m2, m3);
-                let v = read(&code, m1, x) * read(&code, m2, y);
-                write(&mut code, m3, p, v);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
+                mem.write_arg(pop_mode(), pc+3, p1 * p2);
                 pc += 4;
             },
             3 => {
-                let p = code[&(pc+1)];
                 let v = input.recv().await;
-                write(&mut code, pop_mode(), p, v);
-                println!("p={} input={}", p, v);
+                mem.write_arg(pop_mode(), pc+1, v);
                 pc += 2;
             },
             4 => {
-                let p = code[&(pc+1)];
-                let m1 = pop_mode();
-                println!("p={} m1={}", p, m1);
-                output.send(read(&code, m1, p));
+                let v = mem.read_arg(pop_mode(), pc+1);
+                output.send(v);
                 pc += 2;
             },
             5 => {
-                let (p1, p2) = (code[&(pc+1)], code[&(pc+2)]);
-                let p1 = read(&code, pop_mode(), p1);
-                let p2 = read(&code, pop_mode(), p2);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
                 if p1 != 0 {
-                    pc = p2 as usize;
+                    pc = p2;
                 } else {
                     pc += 3;
                 }
             },
             6 => {
-                let (p1, p2) = (code[&(pc+1)], code[&(pc+2)]);
-                let p1 = read(&code, pop_mode(), p1);
-                let p2 = read(&code, pop_mode(), p2);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
                 if p1 == 0 {
-                    pc = p2 as usize;
+                    pc = p2;
                 } else {
                     pc += 3;
                 }
             },
             7 => {
-                let (p1, p2, p3) = (code[&(pc+1)], code[&(pc+2)], code[&(pc+3)]);
-                let p1 = read(&code, pop_mode(), p1);
-                let p2 = read(&code, pop_mode(), p2);
-                //let p3 = access(&mut code, pop_mode(), p3);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
                 if p1 < p2 {
-                    write(&mut code, pop_mode(), p3, 1);
+                    mem.write_arg(pop_mode(), pc+3, 1);
                 } else {
-                    write(&mut code, pop_mode(), p3, 0);
+                    mem.write_arg(pop_mode(), pc+3, 0);
                 }
                 pc += 4;
             },
             8 => {
-                let (p1, p2, p3) = (code[&(pc+1)], code[&(pc+2)], code[&(pc+3)]);
-                let p1 = read(&code, pop_mode(), p1);
-                let p2 = read(&code, pop_mode(), p2);
-                //let p3 = access(&mut code, pop_mode(), p3);
+                let p1 = mem.read_arg(pop_mode(), pc+1);
+                let p2 = mem.read_arg(pop_mode(), pc+2);
                 if p1 == p2 {
-                    write(&mut code, pop_mode(), p3, 1);
+                    mem.write_arg(pop_mode(), pc+3, 1);
                 } else {
-                    write(&mut code, pop_mode(), p3, 0);
+                    mem.write_arg(pop_mode(), pc+3, 0);
                 }
                 pc += 4;
             },
             9 => {
-                let p = code[&(pc+1)];
-                let p = read(&code, pop_mode(), p);
-                rb.set(rb.get() + p);
+                let p = mem.read_arg(pop_mode(), pc+1);
+                mem.update_rb(p);
                 pc += 2;
             },
             99 => break,
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -5,13 +5,23 @@ use common::myasync::*;
 
 async fn part_1() {
     if let Some(Ok(line)) = io::stdin().lock().lines().next() {
-        let (mut s, mut r) = make_channel();
+        let (mut s, r) = make_channel();
         s.send(1);
         let (mut r, _) = run_async(Rc::new(line), r, s).await;
         println!("{}", r.recv().await);
     }
 }
 
+async fn part_2() {
+    if let Some(Ok(line)) = io::stdin().lock().lines().next() {
+        let (mut s, r) = make_channel();
+        s.send(2);
+        let (mut r, _) = run_async(Rc::new(line), r, s).await;
+        println!("{}", r.recv().await);
+    }
+}
+
 fn main() {
-    execute_one(part_1());
+    //execute_one(part_1());
+    execute_one(part_2());
 }
